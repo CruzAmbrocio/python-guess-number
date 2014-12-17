f76757a1ef3613e205a1d12f251e491c5c7095a5
Cruz Ambrocio <cruzambrocio1409@gmail.com>
2014-12-17 13:41:00 -0600

    Nuevo

--- application.py
+++ application.py
@@ -1,3 +1,46 @@
-# Aqui escribe tu codigo
+import random
 
-print "Hello World"
+def Frandom():
+	return random.randint(1,20)
+
+x = Frandom()
+print x
+print "Welcome to Number Generator!"
+print "Can you guess the number? Just try it!"
+print "You have to enter numbers from one to twenty."
+
+count=0
+while count<4:
+	count=count+1
+	print "You have "+str(5-count)+" attempts."
+	try: 
+		y= int(raw_input("Please enter a number: "))
+		if y==x:
+			print "You win!"
+			rest=raw_input("Jugar de nuevo y/n ")
+			if rest=="y":
+				x = Frandom()
+				print x
+				count = 0 
+				pass
+			else:
+				break
+		elif count == 4:
+			print "Game Over!"
+			restGO=raw_input("Jugar de nuevo y/n ")
+			if restGO=="y":
+				x = Frandom()
+				print x
+				count = 0 
+				pass
+			else:
+				print "Gracias"
+				break
+		elif y>x:
+			print "You guessed to high, please try again"
+			
+		elif y<x:
+			print "You guessed to low, please try again"
+			
+	except:		
+		print "Please enter a number. Try again...."
